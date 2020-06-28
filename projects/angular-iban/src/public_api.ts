58751396c8ef276e334647132a29993c5c515284
fundsaccess <li@fundsaccess.com>
2020-06-28 23:00:17 +0200

    chore: added exports

--- projects/angular-iban/src/public_api.ts
+++ projects/angular-iban/src/public_api.ts
@@ -4,3 +4,5 @@
 
 export * from './lib/angular-iban.module';
 export * from './lib/services/validator.service';
+export * from './lib/directives/iban-validator.directive';
+export * from './lib/pipes/iban-formatter.pipe';
