686061a0ee8e778473df94b7addca18447341471
Nikolay Dozmorov <ndozmorov@gmail.com>
2020-10-03 02:00:29 +0700

    Upgrade to Angular 10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AngularIbanModule } from 'projects/angular-iban/src/lib/angular-iban.module'
+import { AngularIbanModule } from 'projects/angular-iban/src/lib/angular-iban.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
