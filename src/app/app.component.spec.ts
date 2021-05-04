ebe5439a59846c43074c1ca1553e805d14399921
Nick Hol <nick@suitablecode.com>
2021-05-04 11:11:38 +0200

    Update to Angular 11 using 'ng update @angular/core @angular/cli'

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,9 @@
-import {TestBed, async, waitForAsync} from '@angular/core/testing';
+import { TestBed, async, waitForAsync } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
